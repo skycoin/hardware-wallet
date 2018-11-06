afcdf3e8e701890de1b3db10efa29403a37d811e
mpsido <mpsido2@gmail.com>
2018-11-06 12:06:05 +0800

    #7 fixing prepare_signature.sh separate libskycoin-crypto.so build from test_skycoin_crypto executable

--- tiny-firmware/bootloader/prepare_signature.sh
+++ tiny-firmware/bootloader/prepare_signature.sh
@@ -1,5 +1,5 @@
 make -C ../../skycoin-api clean
-make -C ../../skycoin-api
+make -C ../../skycoin-api libskycoin-crypto.so
 cp ../../skycoin-api/libskycoin-crypto.so .
 make -C ../../skycoin-api clean
 ln -sf ../../skycoin-api/skycoin_crypto.py .
