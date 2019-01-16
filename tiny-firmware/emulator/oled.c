eb95b9fc23e01851cef1fed235dc859015ce0b39
mpsido <1979371837@qq.com>
2019-01-16 15:10:41 +0800

    Fixing skycoin and trezor license headers

--- tiny-firmware/emulator/oled.c
+++ tiny-firmware/emulator/oled.c
@@ -1,7 +1,8 @@
 /*
- * This file is part of the TREZOR project, https://trezor.io/
+ * This file is part of the Skycoin project, https://skycoin.net/ 
  *
- * Copyright (C) 2017 Saleem Rashid <trezor@saleemrashid.com>
+ * Copyright (C) 2014 Pavol Rusnak <stick@satoshilabs.com>
+ * Copyright (C) 2018-2019 Skycoin Project
  *
  * This library is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
