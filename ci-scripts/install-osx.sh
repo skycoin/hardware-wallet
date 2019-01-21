5f4eb7f0ea5f54c66f1913cb7a7ffc6683cc03a4
Olemis Lang <olemis@simelo.tech>
2019-01-21 03:22:43 -0500

    [ci] refs #57 - Execute brew install every time possible

--- ci-scripts/install-osx.sh
+++ ci-scripts/install-osx.sh
@@ -2,14 +2,15 @@
 
 brew tap skycoin/homebrew-skycoin
 
-brew install \
-  # Install libcheck check C testing framework
-  check \
-  # Install build tools
-  # TODO: Install tools in Debian's build-essentials ?
-  curl unzip git python gcc-arm-none-eabi \
-  # Install SDL
-  sdl2_image sdl2 mesa mesalib-glw
+# Install libcheck check C testing framework
+brew install check
+
+# Install build tools
+# TODO: Install tools in Debian's build-essentials ?
+brew install  curl unzip git python gcc-arm-none-eabi \
+
+# Install SDL
+brew install  sdl2_image sdl2 mesa mesalib-glw
 
 # Install protobuf
 brew install protobuf --with-python
