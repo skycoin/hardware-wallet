58e523bb41bf6cce5f05d15f4fca50b40a4b76d7
Alvaro Denis <dev0003@simelo.tech>
2019-02-05 13:54:30 -0500

    Adding skycoin license file under skycoin-api folder ref #65
     Conflicts:
    	skycoin-api/test_skycoin_crypto.c

--- skycoin-api/test_skycoin_crypto.c
+++ skycoin-api/test_skycoin_crypto.c
@@ -1,3 +1,25 @@
+/*
+ * This file is part of the SKYCOIN project, https://www.skycoin.net/
+ *
+ * Copyright (C) 2018 <contact@skycoin.net>
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "skycoin_crypto.h"
+#include "skycoin_check_signature.h"
+
 #include <stdio.h>
 #include <string.h>
 
