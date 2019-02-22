172e6a6f286fca6e6cbe249acfc9ae1bb745db7e
Alvaro Denis <dev0003@simelo.tech>
2019-02-22 14:53:20 -0500

    be able to mesure the entropy livel of a buffer ref #88

--- tiny-firmware/firmware/fsm_impl.c
+++ tiny-firmware/firmware/fsm_impl.c
@@ -12,6 +12,7 @@
 #include "fsm_impl.h"
 
 #include <stdio.h>
+#include <math.h>
 
 #include <libopencm3/stm32/flash.h>
 
@@ -43,6 +44,67 @@
 #define MNEMONIC_STRENGTH_12 128
 #define MNEMONIC_STRENGTH_24 256
 #define INTERNAL_ENTROPY_SIZE 32
+
+/**
+ * @brief make_histogram create a histogram in plce from bytes
+ * @param bytes source to build the histogram
+ * @param bytes_size size of source bites
+ * @param hist output variable to fill with histogram values
+ * @return the histogram length
+ */
+static uint8_t make_histogram(
+	const uint8_t* const bytes, uint16_t bytes_size, uint8_t *hist) {
+	// NOTE(denisacostaq@gmial.com): byte_posibilities = 2^sizeof(S[0])
+	const uint16_t byte_posibilities = 256;
+	int wherechar[byte_posibilities];
+	for (uint16_t i = 0; i < byte_posibilities; ++i) {
+		wherechar[i] = -1;
+	}
+	{GenerateMnemonic gmMsg;
+		_Static_assert(
+			sizeof(gmMsg.entropy.bytes) < 256, // 2^(size of uint8_t )
+			"entropy.bytes can acumulate more than len, histlen and/or hist[i]");}
+	uint8_t histlen = 0;
+	for (uint8_t i = 0; i < bytes_size; ++i) {
+		if (wherechar[bytes[i]] == -1) {
+			wherechar[bytes[i]] = histlen++;
+		}
+		++hist[wherechar[bytes[i]]];
+	}
+	return histlen;
+}
+
+/**
+ * @brief entropy return the entropy in a giving histogram
+ * @param hist histogram to mesure the entropy from
+ * @param histlen histogram length
+ * @param len amount of symbols
+ * @return the Shannon entropy (bits/symbol)
+ * @sa https://rosettacode.org/wiki/Entropy
+ */
+static float entropy(
+	const uint8_t *const hist, uint8_t histlen, uint16_t len) {
+	float entr = .0f;
+	for (uint8_t i = 0; i < histlen; ++i) {
+		entr -= (float)hist[i]/len * (float)log2((double)hist[i]/len);
+	}
+	return entr;
+}
+
+/**
+ * @brief engout_entropy says if a bytes distribution have engout entropy
+ * @param bytes the bytes to mesur the entropy
+ * @param size the size of bytes
+ * @return an error if not fit minimal entropy required
+ * @sa entropy, make_histogram
+ */
+ErrCode_t engout_entropy(const uint8_t* const bytes, uint16_t size) {
+	uint8_t hist[size];
+	memset(hist, 0, size);
+	uint8_t histlen = make_histogram(bytes, size, hist);
+	float entr = entropy(hist, histlen, size);
+	return entr < 4.f ? ErrFailed : ErrOk;
+}
 
 ErrCode_t msgGenerateMnemonicImpl(GenerateMnemonic* msg) {
 	CHECK_NOT_INITIALIZED_RET_ERR_CODE
