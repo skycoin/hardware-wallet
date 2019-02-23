22b34293b98b422e3a4cc33dea6da5fb2a51678a
Alvaro Denis <dev0003@simelo.tech>
2019-02-22 19:41:39 -0500

    remove `objconv` instalation stage in travis this is not required anymore ref #88

--- ci-scripts/install-osx.sh
+++ ci-scripts/install-osx.sh
@@ -17,17 +17,3 @@ brew install  sdl2_image sdl2 mesa mesalib-glw
 # Install protobuf
 brew install protobuf
 brew install protobuf-c
-
-# Install objconv
-if [ ! -d "objconv" ]; then
-  wget -c http://www.agner.org/optimize/objconv.zip
-  mkdir -p build-objconv
-  unzip objconv.zip -d build-objconv
-
-  cd build-objconv
-  unzip source.zip -d src
-  clang++ -o objconv -O2 src/*.cpp --prefix="$PREFIX"
-fi
-# brew tap hawkw/homebrew-grub
-# brew install objconv
-
