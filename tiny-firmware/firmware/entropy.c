670269c8802a5799f8374d66fac071cf9843a5b7
Alvaro Denis <dev0003@simelo.tech>
2019-03-15 14:42:17 -0400

    use a mixer entropy pool equivalent to https://github.com/skycoin/skycoin/blob/23068ca1d6e11ce05d7c66c5753df578c973a05a/src/cipher/secp256k1-go/secp256_rand.go#L52 ref #121

--- tiny-firmware/firmware/entropy.c
+++ tiny-firmware/firmware/entropy.c
@@ -14,6 +14,51 @@
 #include <string.h>
 
 #include "protob/c/messages.pb.h"
+#include "vendor/skycoin-crypto/tools/sha2.h"
+#include "rng.h"
+
+#define INTERNAL_ENTROPY_SIZE SHA256_DIGEST_LENGTH
+
+static uint8_t entropy_mixer_prev_val[SHA256_DIGEST_LENGTH] = {0};
+
+static void sum_sha256(
+		uint8_t *buffer, size_t buffer_len, uint8_t *out_digest) {
+	SHA256_CTX ctx;
+	sha256_Init(&ctx);
+	sha256_Update(&ctx, buffer, buffer_len);
+	sha256_Final(&ctx, out_digest);
+}
+
+static void two_buffers_sum_sha256(
+		uint8_t *msg1,
+		size_t msg1_len,
+		uint8_t *msg2,
+		size_t msg2_len,
+		uint8_t *out_digest) {
+	size_t two_msgs_len = msg1_len + msg2_len;
+	uint8_t two_msgs[two_msgs_len];
+	memset(two_msgs, 0, two_msgs_len);
+	memcpy(two_msgs, msg1, msg1_len);
+	memcpy(two_msgs + msg1_len, msg2, msg2_len);
+	sum_sha256(two_msgs, two_msgs_len, out_digest);
+}
+
+void mix_256(uint8_t *in, size_t in_len, uint8_t *out_mixed_entropy) {
+	uint8_t val1[SHA256_DIGEST_LENGTH] = {0};
+	sum_sha256(in, in_len, val1);
+	uint8_t val2[SHA256_DIGEST_LENGTH] = {0};
+	two_buffers_sum_sha256(
+				val1,
+				sizeof (val1),
+				entropy_mixer_prev_val,
+				sizeof (entropy_mixer_prev_val),
+				val2);
+	uint8_t val3[SHA256_DIGEST_LENGTH] = {0};
+	two_buffers_sum_sha256(
+		val1, sizeof (val1), val2, sizeof (val2), val3);
+	memcpy(entropy_mixer_prev_val, val3, sizeof (entropy_mixer_prev_val));
+	memcpy(out_mixed_entropy, val2, SHA256_DIGEST_LENGTH);
+}
 
 /**
  * @brief make_histogram create a histogram in place from bytes
--- tiny-firmware/firmware/entropy.h
+++ tiny-firmware/firmware/entropy.h
@@ -14,6 +14,14 @@
 #ifndef __TINYFIRMWARE_FIRMWARE_ENTROPY__
 #define __TINYFIRMWARE_FIRMWARE_ENTROPY__
 
+/**
+ * @brief mix_256 entropy pool mixer.
+ * @param in entropy with 256 bits. 
+ * @param in_len in len.
+ * @param out_mixed_entropy out parama to store the mixed entropy with 256 bits.
+ */
+void mix_256(uint8_t *in, size_t in_len, uint8_t *out_mixed_entropy);
+
 ErrCode_t verify_entropy(const uint8_t* const bytes, uint16_t size);
 
 #endif  // __TINYFIRMWARE_FIRMWARE_ENTROPY__
