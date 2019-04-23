9fff09ad879215029af060218f44236f9b6d3089
therealssj <therealssj@protonmail.com>
2019-04-23 15:31:57 +0530

    use github REST API

--- ci-scripts/verify_protob_hash.sh
+++ ci-scripts/verify_protob_hash.sh
@@ -3,17 +3,14 @@
 # Get commit hash from submodule
 PROTOB_HASH=$(git -C tiny-firmware/protob/ rev-parse HEAD)
 
-# clone protob repository
-mkdir -p tmp/hardware-wallet-protob
-git clone --depth=50 --single-branch --branch master https://github.com/skycoin/hardware-wallet-protob.git tmp/hardware-wallet-protob
-
 # fetch recent 50 commits
 # hardware wallet should not get too behind protob repository
-PROTOB_REMOTE_HASH=$(git log --pretty=oneline | head -50 | cut -c 1-40)
-rm -rf tmp/hardware-wallet-protob
+commits="$(curl https://api.github.com/repos/skycoin/hardware-wallet-protob/commits\?per_page\=50\&sha\=master)"
 
-if ! echo "$PROTOB_REMOTE_HASH" | grep -q "$PROTOB_HASH"; then
-   exit 1
+if echo "$commits" | egrep "\"sha\": \"$PROTOB_HASH\"" > /dev/null
+then
+    echo "success"
+else
+    echo "commit hash not present in recent 50 master commits"
+    exit 1
 fi
-
-exit 0
