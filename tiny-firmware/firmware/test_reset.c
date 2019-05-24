19c9ac76ea10f4a8fcdc138fbae54f1b046f2852
Carlos Gutierrez Ramirez <dev0025@simelo.tech>
2019-05-24 18:09:08 +0000

    [firmware] refs #208 - SEGSIGV running test_reset.c. Check has_*

--- tiny-firmware/firmware/test_reset.c
+++ tiny-firmware/firmware/test_reset.c
@@ -29,15 +29,21 @@ static struct {
 void take_storage_snapshot(void)
 {
     storage_snapshot.has_mnemonic = storage_hasMnemonic();
-    strncpy(storage_snapshot.mnemonic, storage_getMnemonic(), 241);
+    if (storage_snapshot.has_mnemonic) {
+        strncpy(storage_snapshot.mnemonic, storage_getMnemonic(), 241);
+    }
     storage_snapshot.has_label = storage_hasLabel();
-    strncpy(storage_snapshot.label, storage_getLabel(), DEVICE_LABEL_SIZE);
+    if (storage_snapshot.has_label) {
+        strncpy(storage_snapshot.label, storage_getLabel(), DEVICE_LABEL_SIZE);
+    }
     storage_snapshot.has_language = storage_getLanguage() != 0;
     if (storage_snapshot.has_language) {
         strncpy(storage_snapshot.language, storage_getLanguage(), 17);
     }
     storage_snapshot.has_pin = storage_hasPin();
-    strncpy(storage_snapshot.pin, storage_getPin(), 10);
+    if (storage_snapshot.has_pin) {
+        strncpy(storage_snapshot.pin, storage_getPin(), 10);
+    }
     // Ignore has_passphrase since storage_* methods not get it
     storage_snapshot.passphrase_protection = storage_hasPassphraseProtection();
 }
@@ -49,13 +55,17 @@ void assert_storage_matches_snapshot(char* msg)
         ck_assert_msg(strcmp(storage_snapshot.mnemonic, storage_getMnemonic()) == 0, msg);
     }
     ck_assert_msg(storage_snapshot.has_label == storage_hasLabel(), msg);
-    ck_assert_msg(strcmp(storage_snapshot.label, storage_getLabel()) == 0, msg);
+    if (storage_snapshot.has_label) {
+        ck_assert_msg(strcmp(storage_snapshot.label, storage_getLabel()) == 0, msg);
+    }
     ck_assert_msg(storage_snapshot.has_language == (storage_getLanguage() != 0), msg);
     if (storage_snapshot.has_language) {
         ck_assert_msg(strcmp(storage_snapshot.language, storage_getLanguage()) == 0, msg);
     }
     ck_assert_msg(storage_snapshot.has_pin == storage_hasPin(), msg);
-    ck_assert_msg(strcmp(storage_snapshot.pin, storage_getPin()) == 0, msg);
+    if (storage_snapshot.has_pin) {
+        ck_assert_msg(strcmp(storage_snapshot.pin, storage_getPin()) == 0, msg);
+    }
     // Ignore has_passphrase since storage_* methods not get it
     ck_assert_msg(storage_snapshot.passphrase_protection == storage_hasPassphraseProtection(), msg);
 }
@@ -72,6 +82,7 @@ START_TEST(test_reset_invalid_strength)
 
     // Test all combinations
     int display_random_count = 0;
+    int cnt = 0;
     for (display_random = true; display_random_count < 2; ++display_random_count, display_random = false) {
         int passphrase_protection_count = 0;
         for (passphrase_protection = true; passphrase_protection_count < 2; ++passphrase_protection_count, passphrase_protection = false) {
@@ -79,6 +90,7 @@ START_TEST(test_reset_invalid_strength)
             for (pin_protection = true; pin_protection_count < 2; ++pin_protection_count, pin_protection = false) {
                 int skip_backup_count = 0;
                 for (skip_backup = true; skip_backup_count < 2; ++skip_backup_count, skip_backup = false) {
+		    fprintf(stderr, "Loop %d", ++cnt);
                     reset_init(display_random, 160, passphrase_protection, pin_protection, "english", "lbl", skip_backup);
                     char testMsg[256];
                     sprintf(testMsg, "Invoke reset_init with display_random=%d strength=%d passphrase=%d pin=%d lang=%s label=%s skip_backup=%d",
