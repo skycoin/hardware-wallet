adeaaa695caeea8f35a86d212714bf31d76f9ef3
Alvaro Denis <dev0003@simelo.tech>
2019-07-05 18:20:02 -0400

    roolback a lot of unwanted changes for this issue ref #212

--- ci-scripts/install-linux.sh
+++ ci-scripts/install-linux.sh
@@ -4,11 +4,15 @@ set -x
 
 sudo apt-get update
 
-sudo apt-get install -qq gcc-6 g++-6
 # Install libcheck check C testing framework
-wget -c https://github.com/libcheck/check/releases/download/0.12.0/check-0.12.0.tar.gz
-tar -xzf check-0.12.0.tar.gz
-cd check-0.12.0 && ./configure --prefix=/usr --disable-static && make && sudo make install && cd -
+wget https://github.com/libcheck/check/releases/download/0.12.0/check-0.12.0.tar.gz
+tar xvf check-0.12.0.tar.gz
+cd check-0.12.0
+autoreconf --install
+./configure
+make
+cp src/.libs/*.so* src
+cd -
 
 # Install build tools
 sudo apt-get install -y build-essential curl unzip git python3 python3-pip python-protobuf gcc-arm-none-eabi
@@ -16,5 +20,3 @@ sudo apt-get install -y build-essential curl unzip git python3 python3-pip pytho
 # Install SDL
 sudo apt-get install -y libegl1-mesa-dev libgles2-mesa-dev libsdl2-dev libsdl2-image-dev
 
-##INstall 
-sudo apt-get install lib32z1 lib32ncurses5 -y
