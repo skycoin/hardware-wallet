a327d52c00796955a0d82127f3386e36c6a05ba4
Macrosrider <zubrytsky@ucu.edu.ua>
2019-07-30 18:40:22 +0300

    Fixed combine.py to previous version

--- tiny-firmware/bootloader/combine/prepare.py
+++ tiny-firmware/bootloader/combine/prepare.py
@@ -1,15 +1,8 @@
 #!/usr/bin/env python
 from __future__ import print_function
-import platform
 
-if platform.system().split("_")[0] == 'MSYS':
-    with open("bl.bin", 'rb') as f1:
-        bl = f1.read().decode('utf8', 'replace')
-    with open("fw.bin", 'rb') as f2:
-        fw = f2.read().decode('utf8', 'replace')
-else:
-    bl = open('bl.bin').read()
-    fw = open('fw.bin').read()
+bl = open('bl.bin').read()
+fw = open('fw.bin').read()
 combined = bl + fw[:256] + (32768-256)*'\x00' + fw[256:]
 
 open('combined.bin', 'w').write(combined)
