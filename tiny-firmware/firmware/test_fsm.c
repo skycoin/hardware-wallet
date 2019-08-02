46127af163f3852314ff10d13628c15a6a4e410a
stdevAdrianPaez <adriantpaez@gmail.com>
2019-08-02 19:57:11 -0400

    [firmware] Test for long transactions

--- tiny-firmware/firmware/test_fsm.c
+++ tiny-firmware/firmware/test_fsm.c
@@ -1345,6 +1345,312 @@ START_TEST(test_msgTransactionSign10)
 }
 END_TEST
 
+START_TEST(test_msgTransactionSign11)
+{
+    // Set mnemonic
+    SetMnemonic mnemonic = SetMnemonic_init_zero;
+    char mnemonic_str[] = { "network hurdle trash obvious soccer sunset side merit horn author horn you" };
+    memcpy(mnemonic.mnemonic,mnemonic_str, sizeof(mnemonic_str));
+    ck_assert_int_eq(msgSetMnemonicImpl(&mnemonic), ErrOk);
+
+    // Send SignTx message
+    SignTx sing_tx = SignTx_init_default;
+    sign_tx.outputs_count = 14;
+    sign_tx.inputs_count = 14;
+    sign_tx.has_coin_name = true;
+    sign_tx.coin_name = "Skycoin";
+    sign_tx.has_version = true;
+    sign_tx.version = 1;
+    sign_tx.has_lock_time = true;
+    sign_tx.lock_time = 3;
+    sign._tx.has_tx_hash = true;
+    sign_tx.tx_hash = "8cbdfbc7aa9975904b805152a816a897d7f8b652806fd9ceec8822b096c6fc18";
+    TxRequest response = TxRequest_init_default;
+    ck_assert_int_eq(msgSignTxImpl(&sign_tx, &response), ErrOk);
+    ck_assert_int_eq(response.request_type,TxRequest_RequestType_TXINPUT);
+    ck_assert_int_eq(response.has_details, true);
+    ck_assert_int_eq(response.details.request_index, 1);
+    
+    // Init TxAck message for inputs
+    TxAck tx_ack = TxAck_init_default;
+    tx_ack.has_tx = true;
+    tx_ack.tx.has_version = true;
+    tx_ack.tx.version = 1;
+    tx_ack.tx.inputs_count = 7;
+    tx_ack.tx.outputs_count = 0;
+    tx_ack.tx.has_lock_time = true;
+    tx_ack.tx.lock_time = 3;
+
+    // Send inputs 0-6
+    tx_ack.tx.inputs = {
+        {.hashIn = "941a422ed8b17ae9dcbf942ace143f77c26a9c02d2e6395b46d50d1079ba4b00",
+         .address_n_count = 1,
+         .address_n = {0} },
+        {.hashIn = "1c7afa90dcd10063722c0cf8981c52bc715a6d522c4fcc92cd743e2120e866f2",
+         .address_n_count = 1,
+         .address_n = {0} },
+        {.hashIn = "1e06ce57e449a3744a58d115da99f375926b30a4aaa62500f36400c479c86026",
+         .address_n_count = 1,
+         .address_n = {0} },
+        {.hashIn = "c1dca5b6e34dbc8b8a87cae3074e1c24e555c8300043975982b3754c25fa31ef",
+         .address_n_count = 1,
+         .address_n = {0} },
+        {.hashIn = "516d5dd0465474b53520c2c7377f293823c36740b10a6cace58bcf53cabf91f2",
+         .address_n_count = 1,
+         .address_n = {0} },
+        {.hashIn = "69c010a4f512ef9a1ed73e8da5e57e7fdb11b9cadfa42babaf393c528d05b372",
+         .address_n_count = 1,
+         .address_n = {0} },
+        {.hashIn = "f7aeffdbfad94333c5198149bc1ad2a02acabeb3e67834f2f4c8aa2cac20248d",
+         .address_n_count = 1,
+         .address_n = {0} }
+    };
+    response = TxRequest_init_default;
+    ck_assert_int_eq(msgTxAckImpl(&tx_ack,&response), ErrOk);
+    ck_assert_int_eq(response.request_type,TxRequest_RequestType_TXINPUT);
+    ck_assert_int_eq(response.has_details, true);
+    ck_assert_int_eq(response.details.request_index, 2);
+
+    // Send inputs 7-13
+    tx_ack.tx.inputs = {
+        {.hashIn = "6e99ca948cb8d50dc8708d77498fccc65ca2e6e8d6192c4b178999a6ed403752",
+         .address_n_count = 1,
+         .address_n = {0} },
+        {.hashIn = "6bb7c8ec3654b5f4718988606e15947ac45fd8ac255a6838c08c926a199c412d",
+         .address_n_count = 1,
+         .address_n = {0} },
+        {.hashIn = "19c3f8ef155b363cfc9e508de5930aad477e3ffd974ac90fd53b5eb0853cf29a",
+         .address_n_count = 1,
+         .address_n = {0} },
+        {.hashIn = "0516f33e59a97587135ee4350c8edff3a196415f31e3233579c29c505d6efa2c",
+         .address_n_count = 1,
+         .address_n = {0} },
+        {.hashIn = "1d6a5a6fa9af1acf4ad8bf4348369adcaf61bab84b82697db5f733f60f91337b",
+         .address_n_count = 1,
+         .address_n = {0} },
+        {.hashIn = "d7a958d8e7edbab356b68f257e3b57d44d35e22f1fcfbee70116d502fa48e390",
+         .address_n_count = 1,
+         .address_n = {0} },
+        {.hashIn = "fc8542d202ee8c57a8f91058aaaf852a3db695248f94302f3f76f29e42d284c1",
+         .address_n_count = 1,
+         .address_n = {0} }
+    }
+    response = TxRequest_init_default;
+    ck_assert_int_eq(msgTxAckImpl(&tx_ack,&response), ErrOk);
+    ck_assert_int_eq(response.request_type,TxRequest_RequestType_TXOUTPUT);
+    ck_assert_int_eq(response.has_details, true);
+    ck_assert_int_eq(response.details.request_index, 3);
+
+    // Init TxAck message for outputs
+    tx_ack = TxAck_init_default;
+    tx_ack.has_tx = true;
+    tx_ack.tx.has_version = true;
+    tx_ack.tx.version = 1;
+    tx_ack.tx.inputs_count = 0;
+    tx_ack.tx.outputs_count = 7;
+    tx_ack.tx.has_lock_time = true;
+    tx_ack.tx.lock_time = 3;
+
+    // Send outputs 0-6
+    tx_ack.tx.outputs = {
+        {.address = "2EU3JbveHdkxW6z5tdhbbB2kRAWvXC2pLzw",
+         .address_n_count = 0,
+         .coins = 2,
+         .hours = 255},
+        {.address = "zC8GAQGQBfwk7vtTxVoRG7iMperHNuyYPs",
+         .address_n_count = 0,
+         .coins = 2,
+         .hours = 255},
+        {.address = "28L2fexvThTVz6e2dWUV4pSuCP8SAnCUVku",
+         .address_n_count = 0,
+         .coins = 2,
+         .hours = 255},
+        {.address = "2NckPkQRQFa5E7HtqDkZmV1TH4HCzR2N5J6",
+         .address_n_count = 0,
+         .coins = 2,
+         .hours = 255},
+        {.address = "2ARhYQsMmMZuw5LPmZQvyWoTm1VUH8kSZ14",
+         .address_n_count = 0,
+         .coins = 2,
+         .hours = 255},
+        {.address = "FjWaGnwGTswo7TegsB9KikqcxGHy8zHTS1",
+         .address_n_count = 0,
+         .coins = 2,
+         .hours = 255},
+        {.address = "26GcGAkdrXYkPVKkHvy5FQWAeLVVVGSmz6V",
+         .address_n_count = 0,
+         .coins = 2,
+         .hours = 255}
+    }
+    response = TxRequest_init_default;
+    ck_assert_int_eq(msgTxAckImpl(&tx_ack,&response), ErrOk);
+    ck_assert_int_eq(response.request_type,TxRequest_RequestType_TXOUTPUT);
+    ck_assert_int_eq(response.has_details, true);
+    ck_assert_int_eq(response.details.request_index, 4);
+
+    // Send outputs 7-13
+    tx_ack.tx.outputs = {
+        {.address = "obaHadRxLaz8Laoxi8TA799ycHXNK2hgdz",
+         .address_n_count = 0,
+         .coins = 2,
+         .hours = 255},
+        {.address = "26jybLrsXXWjwBdmR2eo4c25hYGMipLEjTr",
+         .address_n_count = 0,
+         .coins = 2,
+         .hours = 255},
+        {.address = "2CgDFkD43CUpQot3e76iuoB2BxCGXZ6xn38",
+         .address_n_count = 0,
+         .coins = 2,
+         .hours = 255},
+        {.address = "cMdZRECEGhrBqtKvuB9XW2EK9tqi4znNhN",
+         .address_n_count = 0,
+         .coins = 2,
+         .hours = 255},
+        {.address = "ouE9prS4XMhVwQa5gwW4hQuiAGamMcJmK5",
+         .address_n_count = 0,
+         .coins = 2,
+         .hours = 255},
+        {.address = "ZyLHjEdTbxnYHZC2U4ZNqSuZBUh74Sjh9v",
+         .address_n_count = 0,
+         .coins = 2,
+         .hours = 255},
+        {.address = "2eZCcsBQVooxUStrCKEZ4TLZzbGa7tx7Cwa",
+         .address_n_count = 0,
+         .coins = 2,
+         .hours = 255}
+    }
+    response = TxRequest_init_default;
+    ck_assert_int_eq(msgTxAckImpl(&tx_ack,&response), ErrOk);
+    ck_assert_int_eq(response.request_type,TxRequest_RequestType_TXINPUT);
+    ck_assert_int_eq(response.has_details, true);
+    ck_assert_int_eq(response.details.request_index, 5);
+
+    // Init TxAck message for inputs
+    TxAck tx_ack = TxAck_init_default;
+    tx_ack.has_tx = true;
+    tx_ack.tx.has_version = true;
+    tx_ack.tx.version = 1;
+    tx_ack.tx.inputs_count = 7;
+    tx_ack.tx.outputs_count = 0;
+    tx_ack.tx.has_lock_time = true;
+    tx_ack.tx.lock_time = 3;
+
+    // Send inputs 0-6
+    tx_ack.tx.inputs = {
+        {.hashIn = "941a422ed8b17ae9dcbf942ace143f77c26a9c02d2e6395b46d50d1079ba4b00",
+         .address_n_count = 1,
+         .address_n = {0} },
+        {.hashIn = "1c7afa90dcd10063722c0cf8981c52bc715a6d522c4fcc92cd743e2120e866f2",
+         .address_n_count = 1,
+         .address_n = {0} },
+        {.hashIn = "1e06ce57e449a3744a58d115da99f375926b30a4aaa62500f36400c479c86026",
+         .address_n_count = 1,
+         .address_n = {0} },
+        {.hashIn = "c1dca5b6e34dbc8b8a87cae3074e1c24e555c8300043975982b3754c25fa31ef",
+         .address_n_count = 1,
+         .address_n = {0} },
+        {.hashIn = "516d5dd0465474b53520c2c7377f293823c36740b10a6cace58bcf53cabf91f2",
+         .address_n_count = 1,
+         .address_n = {0} },
+        {.hashIn = "69c010a4f512ef9a1ed73e8da5e57e7fdb11b9cadfa42babaf393c528d05b372",
+         .address_n_count = 1,
+         .address_n = {0} },
+        {.hashIn = "f7aeffdbfad94333c5198149bc1ad2a02acabeb3e67834f2f4c8aa2cac20248d",
+         .address_n_count = 1,
+         .address_n = {0} }
+    };
+    response = TxRequest_init_default;
+    ck_assert_int_eq(msgTxAckImpl(&tx_ack,&response), ErrOk);
+    ck_assert_int_eq(response.request_type,TxRequest_RequestType_TXINPUT);
+    ck_assert_int_eq(response.has_details, true);
+    ck_assert_int_eq(response.details.request_index, 6);
+    ck_assert_int_eq(response.sign_result_count, 7);
+
+    char addresses[][7] = { "2EU3JbveHdkxW6z5tdhbbB2kRAWvXC2pLzw",
+                            "zC8GAQGQBfwk7vtTxVoRG7iMperHNuyYPs",
+                            "28L2fexvThTVz6e2dWUV4pSuCP8SAnCUVku",
+                            "2NckPkQRQFa5E7HtqDkZmV1TH4HCzR2N5J6",
+                            "2ARhYQsMmMZuw5LPmZQvyWoTm1VUH8kSZ14",
+                            "FjWaGnwGTswo7TegsB9KikqcxGHy8zHTS1",
+                            "26GcGAkdrXYkPVKkHvy5FQWAeLVVVGSmz6V"};
+    char messages[64][7] = {    "2b52eb40abc3ac8c19bd31fcb37ff2d238ecffa2d39d1d4b64a1bf593f7f3758",
+                                "50054c5dfe0d9cfe7d87d0ce01f576cc1601ab1cf02e3c7159a0405e62593b0e",
+                                "581de7e15946d30dd36160156b9977718f6c63645bf84fc608406759e4643382",
+                                "b674042cdb8e94b3c0959dc014c27827dc641ab7c3336ec96732e619fe348ca4",
+                                "7aaca187e055cc57e6356e009b33aea5e614c5936938ed58f94173f7387c0855",
+                                "3ffd187d8d686f8d65e2f00e869c5ccceb53bde9849dd9768c9a64f02d401b80",
+                                "8466467651b898a763b313e7d9bbbd4ef42cb4e95a2e1c7d0a10c173b9277ff9"};
+    SkycoinCheckMessageSignature msg_s = SkycoinCheckMessageSignature_init_zero;
+    for (uint8_t i = 0; i < 7; i++){
+        memcpy(msg_s.address, sddresses[i], sizeof(addresses[i]));
+        memcpy(msg_s.message, messages[i], sizeof(messages[i]));
+        memcpy(msg_s.signature, response.sign_result[i], sizeof(response.sign_result[i]));
+        Failure failure_resp = Failure_init_default;
+        Success success_resp = Success_init_default;
+        ErrCode_t check_sign = msgSkycoinCheckMessageSignatureImpl(
+            &msg_s, &success_resp, &failure_resp);
+        ck_assert_int_eq(check_sign, ErrOk);
+    }
+
+    // Send inputs 7-13
+    tx_ack.tx.inputs = {
+        {.hashIn = "6e99ca948cb8d50dc8708d77498fccc65ca2e6e8d6192c4b178999a6ed403752",
+         .address_n_count = 1,
+         .address_n = {0} },
+        {.hashIn = "6bb7c8ec3654b5f4718988606e15947ac45fd8ac255a6838c08c926a199c412d",
+         .address_n_count = 1,
+         .address_n = {0} },
+        {.hashIn = "19c3f8ef155b363cfc9e508de5930aad477e3ffd974ac90fd53b5eb0853cf29a",
+         .address_n_count = 1,
+         .address_n = {0} },
+        {.hashIn = "0516f33e59a97587135ee4350c8edff3a196415f31e3233579c29c505d6efa2c",
+         .address_n_count = 1,
+         .address_n = {0} },
+        {.hashIn = "1d6a5a6fa9af1acf4ad8bf4348369adcaf61bab84b82697db5f733f60f91337b",
+         .address_n_count = 1,
+         .address_n = {0} },
+        {.hashIn = "d7a958d8e7edbab356b68f257e3b57d44d35e22f1fcfbee70116d502fa48e390",
+         .address_n_count = 1,
+         .address_n = {0} },
+        {.hashIn = "fc8542d202ee8c57a8f91058aaaf852a3db695248f94302f3f76f29e42d284c1",
+         .address_n_count = 1,
+         .address_n = {0} }
+    };
+    response = TxRequest_init_default;
+    ck_assert_int_eq(msgTxAckImpl(&tx_ack,&response), ErrOk);
+    ck_assert_int_eq(response.request_type,TxRequest_RequestType_TXFINISHED);
+    ck_assert_int_eq(response.has_details, true);
+    ck_assert_int_eq(response.details.request_index, 7);
+    ck_assert_int_eq(response.sign_result_count, 7);
+
+    char addresses[][7] = { "obaHadRxLaz8Laoxi8TA799ycHXNK2hgdz",
+                            "26jybLrsXXWjwBdmR2eo4c25hYGMipLEjTr",
+                            "2CgDFkD43CUpQot3e76iuoB2BxCGXZ6xn38",
+                            "cMdZRECEGhrBqtKvuB9XW2EK9tqi4znNhN",
+                            "ouE9prS4XMhVwQa5gwW4hQuiAGamMcJmK5",
+                            "ZyLHjEdTbxnYHZC2U4ZNqSuZBUh74Sjh9v",
+                            "2eZCcsBQVooxUStrCKEZ4TLZzbGa7tx7Cwa"};
+    char messages[64][7] = {    "f907007d5ece78a762661b677fd1fc18f96a6273929ec6fb81113c2e0a450305",
+                                "dfc2021ca9bfb817261fe3acf8edcbd9a1bf59b51f92cb58748215daaea0aa5c",
+                                "399c472426f4930745bc59fcde905258dd4add4dabcb5b8fbae28abb7d29a9ac",
+                                "b68a1c52bbd8ea294a6c8ca8545b90006dd69ea3b280501edbef3e52664dc9c2",
+                                "a79671d5118392a1705cc1810b170bb2d63e418cb6c9a5bcc726f8228772f2d7",
+                                "35120affcbc2e43dfee456678ac5f7e98ecf48c70765b2921ec6ccfbd8b9bbba",
+                                "eed09fe8a2b2e59e5e0d318e4afb29631ba4a5241593359da890fc45396c348a"};
+    SkycoinCheckMessageSignature msg_s = SkycoinCheckMessageSignature_init_zero;
+    for (uint8_t i = 0; i < 7; i++){
+        memcpy(msg_s.address, sddresses[i], sizeof(addresses[i]));
+        memcpy(msg_s.message, messages[i], sizeof(messages[i]));
+        memcpy(msg_s.signature, response.sign_result[i], sizeof(response.sign_result[i]));
+        Failure failure_resp = Failure_init_default;
+        Success success_resp = Success_init_default;
+        ErrCode_t check_sign = msgSkycoinCheckMessageSignatureImpl(
+            &msg_s, &success_resp, &failure_resp);
+        ck_assert_int_eq(check_sign, ErrOk);
+    }
+}
+END_TEST
+
 // define test cases
 TCase* add_fsm_tests(TCase* tc)
 {
@@ -1387,5 +1693,6 @@ TCase* add_fsm_tests(TCase* tc)
     tcase_add_test(tc, test_msgTransactionSign8);
     tcase_add_test(tc, test_msgTransactionSign9);
     tcase_add_test(tc, test_msgTransactionSign10);
+    tcase_add_test(tc, test_msgTransactionSign11);
     return tc;
 }
