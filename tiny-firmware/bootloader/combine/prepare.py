51c7b94f4691803da09eb2a922cbe911f94ad1aa
Macrosrider <zubrytsky@ucu.edu.ua>
2019-08-13 16:48:36 +0300

    Added universal way combining firmware & bootloader for Windows and UNIX

--- tiny-firmware/bootloader/combine/prepare.py
+++ tiny-firmware/bootloader/combine/prepare.py
@@ -1,26 +1,15 @@
 #!/usr/bin/env python
 from __future__ import print_function
-import platform
 
-print(platform.system())
+with open('bl.bin', 'rb') as f1:
+    bl = f1.read()
 
-if platform.system().split('_')[0] == "MSYS":
-    with open('bl.bin', 'rb') as f1:
-        bl = f1.read()
+with open('fw.bin', 'rb') as f2:
+    fw = f2.read()
 
-    with open('fw.bin', 'rb') as f2:
-        fw = f2.read()
+combined = bl + fw[:256] + (32768-256)*'\x00'.encode('utf-8') + fw[256:]
 
-    combined = bl + fw[:256] + (32768-256)*'\x00'.encode('utf-8') + fw[256:]
-
-    open('combined.bin', 'wb').write(combined)
-else:
-    bl = open('bl.bin').read()
-    fw = open('fw.bin').read()
-
-    combined = bl + fw[:256] + (32768-256)*'\x00' + fw[256:]
-
-    open('combined.bin', 'w').write(combined)
+open('combined.bin', 'wb').write(combined)
 
 print('bootloader : %d bytes' % len(bl))
 print('firmware   : %d bytes' % len(fw))
