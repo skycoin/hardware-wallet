1dfaa2d678c2b05d1cdfb40bcf3048749ef15027
Mark <zubrytsky@ucu.edu.ua>
2019-08-13 16:28:55 +0300

    Added firmware and bootloader build on Windows

--- tiny-firmware/bootloader/combine/prepare.py
+++ tiny-firmware/bootloader/combine/prepare.py
@@ -1,11 +1,26 @@
 #!/usr/bin/env python
 from __future__ import print_function
+import platform
 
-bl = open('bl.bin').read()
-fw = open('fw.bin').read()
-combined = bl + fw[:256] + (32768-256)*'\x00' + fw[256:]
+print(platform.system())
 
-open('combined.bin', 'w').write(combined)
+if platform.system().split('_')[0] == "MSYS":
+    with open('bl.bin', 'rb') as f1:
+        bl = f1.read()
+
+    with open('fw.bin', 'rb') as f2:
+        fw = f2.read()
+
+    combined = bl + fw[:256] + (32768-256)*'\x00'.encode('utf-8') + fw[256:]
+
+    open('combined.bin', 'wb').write(combined)
+else:
+    bl = open('bl.bin').read()
+    fw = open('fw.bin').read()
+
+    combined = bl + fw[:256] + (32768-256)*'\x00' + fw[256:]
+
+    open('combined.bin', 'w').write(combined)
 
 print('bootloader : %d bytes' % len(bl))
 print('firmware   : %d bytes' % len(fw))
