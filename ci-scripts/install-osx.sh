74a8ac0be386dfb7941692075c6d486933fdaf45
Macrosrider <zubrytsky@ucu.edu.ua>
2020-01-09 12:54:32 +0200

    .

--- ci-scripts/install-osx.sh
+++ ci-scripts/install-osx.sh
@@ -10,9 +10,8 @@ brew install check
 # Install build tools
 # TODO: Install tools in Debian's build-essentials ?
 brew install md5sha1sum curl unzip  gcc-arm-none-eabi
-brew install python3 --configure="--with-pydebug --with-openssl=$(brew --prefix openssl)"
+brew install python3
 brew unlink python@2
-brew link python@3
 
 # Install SDL
 brew install  sdl2_image sdl2 mesa mesalib-glw
