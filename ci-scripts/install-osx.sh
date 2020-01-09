76aca11437f8cac1896db9967f2a98611d66d7d3
Macrosrider <zubrytsky@ucu.edu.ua>
2020-01-09 12:05:36 +0200

    Fixed link error

--- ci-scripts/install-osx.sh
+++ ci-scripts/install-osx.sh
@@ -11,7 +11,8 @@ brew install check
 # TODO: Install tools in Debian's build-essentials ?
 brew install md5sha1sum curl unzip  gcc-arm-none-eabi
 brew install python3 --configure="--with-pydebug --with-openssl=$(brew --prefix openssl)"
-brew link --overwrite python3
+brew unlink python@2
+brew link python@3
 
 # Install SDL
 brew install  sdl2_image sdl2 mesa mesalib-glw
