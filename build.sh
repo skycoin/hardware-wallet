d95764174476ac0aebedb3eff041431729ddd85b
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2020-10-06 17:27:00 +0300

    Add `git submodule update` to build.sh

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@
 
 ORG="skycoinproject"
 
+git submodule update --init --recursive
 docker pull $ORG/skywallet && \
 docker run --rm -it -v $(pwd):/hardware-wallet:Z -w /hardware-wallet $ORG/skywallet make full-firmware
